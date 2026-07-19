7ca943d574758e204a752576af704f22b2869639
Louise Love Camille <110845941+LouiseLoveYou@users.noreply.github.com>
2026-07-19 03:25:40 +0200

    Add files via upload

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksella <ksella@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/15 17:34:14 by username          #+#    #+#             */
+/*   Updated: 2026/07/15 23:12:41 by ksella           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < nb && src[j])
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+int	main(void)
+{
+	char			dest[155] = "tung tung tung ";
+	char			src[] = "sahur!\n";
+	unsigned int	nb;
+
+	nb = 3;
+	ft_strncat(dest, src, nb);
+	printf("%s", dest);
+	return (0);
+}
